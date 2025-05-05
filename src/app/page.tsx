cfb44ccc53efc9dc3e4f7e17efe262980bf6f123
Kunal Agrawal <kunal.py3@gmail.com>
2025-05-05 10:04:11 +0530

    feat: implement authentication check and conditional rendering in Kanban page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import { KanbanBoard } from "@/components/KanbanBoard";
-import React from "react";
+"use client";
 
-const page = () => {
-  return (
-    <>
-      <KanbanBoard />
-    </>
-  );
+import React, { useEffect } from "react";
+
+import { KanbanBoard } from "@/components/KanbanBoard";
+import { useAuthContext } from "@/hooks/AuthContext";
+import { useRouter } from "next/navigation";
+
+const Page = () => {
+  const router = useRouter();
+  const { user, loading } = useAuthContext();
+  useEffect(() => {
+    if (user === undefined && loading === false) {
+      router.replace("/auth");
+    }
+  }, [loading, router, user]);
+  return <>{loading === false && user && <KanbanBoard />}</>;
 };
 
-export default page;
+export default Page;
